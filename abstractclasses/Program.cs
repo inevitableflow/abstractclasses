b04284810a0066c4699e8b598be54f712f4a585b
inevitableflow <raquelcastoldi16@gmail.com>
2023-02-17 14:30:21 -0700

    Add project files.

---
+++ abstractclasses/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstractclasses
+{
+    public abstract class Animal
+    {
+        static void Main(string[] args)
+        {
+        }
+        string Name { get; set; }
+        string Colour { get; set; }
+        int Age { get; set; }
+
+        internal abstract void Eat();
+
+    }
+
+}
